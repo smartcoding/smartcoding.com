d91579446d2cd41f4c3ac7c829b316ac34dd71df
sergeylukin <sergeylukin@insomnia247.nl>
2019-03-31 14:36:26 +0200

    Add Tawk.to integration

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,13 @@
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5ca0b3a41de11b6e3b06151f/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+
 const Index = () => (
   <div>
     <img src="/static/logo.png" />
