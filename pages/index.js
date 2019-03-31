f3f68de07caf6fcde840c32cd769b69f804a88ff
sergeylukin <sergeylukin@insomnia247.nl>
2019-03-31 14:35:16 +0200

    Add homepage

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+const Index = () => (
+  <div>
+    <img src="/static/logo.png" />
+      <style jsx>{`
+        img {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          margin: -112px 0 0 -231px;
+        }
+      `}</style>
+  </div>
+)
+
+export default Index
